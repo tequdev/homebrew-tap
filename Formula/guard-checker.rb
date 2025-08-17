59b6c73bcb9f7f991df3d957df556510a3e8636d
tequ <git@tequ.dev>
2025-08-17 23:37:58 +0900

    update guard-checker

--- Formula/guard-checker.rb
+++ Formula/guard-checker.rb
@@ -4,8 +4,8 @@
 class GuardChecker < Formula
   desc "Standalone repo for the Hooks Guard checker code (the code that runs in SetHook)."
   homepage ""
-  url "https://github.com/tequdev/guard-checker/archive/refs/tags/0.1.0.tar.gz"
-  sha256 "890a9e0b399bdc9f8071f90ff7d26d1f10862730dc97ec84f1c944c4294298d3"
+  url "https://github.com/tequdev/guard-checker/archive/refs/tags/0.2.0.tar.gz"
+  sha256 "ab972239723c99f113d5d6f7f40ac6116eeed92a1ac5cd37263c0a7114c84166"
   license ""
 
   # depends_on "cmake" => :build
